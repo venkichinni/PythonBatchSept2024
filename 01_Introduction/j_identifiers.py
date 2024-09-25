37507d81c4446abdc3927a8091c99b1c7271c88f
venkichinni <venkichinni27@gmail.com>
2024-09-25 02:30:19 +0000

    checkin

---
+++ 01_Introduction/j_identifiers.py
@@ -0,0 +1,32 @@
+
+
+"""
+1) Keywords should not use
+2) first char is A-Z, a-z,_
+3) remaining chars: a-z,A-Z, _, 0-9
+
+"""
+
+
+true_value=123
+none_result="nonthing"
+
+name="python"
+name_of_student="Hari K"
+First_name="Hari"
+first___child="yellow"
+
+PI=3.146
+DOZON=12  #Constant variable 
+
+###Bad practises below
+
+
+# $name="prinyanka"
+# name-of-student="Hari K"
+# name of student ="hari"
+# 1st_name="Hari"
+
+
+print("__name__ =", __name__) 
+print("__file__ =", __file__)
