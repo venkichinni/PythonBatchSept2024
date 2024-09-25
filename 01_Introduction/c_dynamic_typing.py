59ae2f42fa0e9f24c7a9d7b02c265056d4e67e17
venkichinni <venkichinni27@gmail.com>
2024-09-25 00:16:06 +0000

    add files

---
+++ 01_Introduction/c_dynamic_typing.py
@@ -0,0 +1,70 @@
+##python is Dymanic typed language no need declare variable
+
+
+num1 = 100
+
+print(num1)
+type(num1)
+
+print(type(num1))
+
+
+print('num1 =', num1)
+print("num1 =", num1)
+
+
+print("num1 =", num1, "type(num1) =", type(num1))
+
+
+num1 = 200
+print("num1 =", num1, "type(num1) =", type(num1))
+
+num1 = 200.
+print("num1 =", num1, "type(num1) =", type(num1))
+
+num1 = 200.0
+print("num1 =", num1, "type(num1) =", type(num1))
+
+num1 = 200.0,
+print("num1 =", num1, "type(num1) =", type(num1))
+print()
+
+
+num1 = .200
+print("num1 =", num1, "type(num1) =", type(num1))
+
+num1 = .200j
+print("num1 =", num1, "type(num1) =", type(num1))
+
+num1 = 10 + .200j
+print("num1 =", num1, "type(num1) =", type(num1))
+print()
+
+
+num1 = "10 + .200j"
+print("num1 =", num1, "type(num1) =", type(num1))
+
+num1 = "3"
+print("num1 =", num1, "type(num1) =", type(num1))
+
+num1 = "three"
+print("num1 =", num1, "type(num1) =", type(num1))
+print()
+
+num1 = True
+print("num1 =", num1, "type(num1) =", type(num1))
+
+num1 = False
+print("num1 =", num1, "type(num1) =", type(num1))
+
+
+num1 = "true"
+print("num1 =", num1, "type(num1) =", type(num1))
+print()
+
+
+num1 = None
+print("num1 =", num1, "type(num1) =", type(num1))
+
+num1 = "None"
+print("num1 =", num1, "type(num1) =", type(num1))
