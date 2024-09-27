c69ba910bcb82073f322f65501e379eb8be470eb
venkichinni <venkichinni27@gmail.com>
2024-09-27 02:11:27 +0000

    savings_bank

---
+++ 02_Basics/h_savings_bank.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+
+####savings bank
+
+##Initial Balance=0
+
+
+balance = 0
+initial_min_balance = 1500
+salary_credited = 3300
+online_purchase = 33.33
+house_rent_paid = 1500
+
+display_balance = (balance + initial_min_balance+salary_credited+online_purchase+house_rent_paid)
+
+print(display_balance)
