d0760455aa3e84b5eaf8952c55bd295428950852
venkichinni <venkichinni27@gmail.com>
2024-09-27 02:31:28 +0000

    simple intrest caliculator

---
+++ 02_Basics/i_bank_loan.py
@@ -0,0 +1,28 @@
+#!usr/bin/python3
+### simple intrest caliculator
+
+
+
+
+
+## input feilds
+
+
+principal=float(input("Enter initial principal bal amount (P) : "))
+
+rate=float(input("Enter Annual intrest rate(r). please enter in decimals ex: 0.06 for 6% :"))
+
+time = float(input("enter time in years (t) : "))
+
+
+final_amount = principal * (1 + (rate * time))
+
+
+print (" the final amount for " + str(time) + "years will be :", final_amount)
+
+
+
+
+
+
+           
